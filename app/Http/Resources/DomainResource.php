be7532cd27b8fc0bc76e0e28d4fcd6e30999bcff
Jusdepixel <mandy@jusdepixel.fr>
2023-03-18 20:01:10 +0100

    ci: add resources module Domain

---
+++ app/Http/Resources/DomainResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DomainResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'icon' => $this->icon,
+            'secure' => $this->secure,
+            'default' => $this->default,
+        ];
+    }
+}
