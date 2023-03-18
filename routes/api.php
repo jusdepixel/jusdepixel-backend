8b965aef4f09997d51a3606f303767b894b79a91
Jusdepixel <mandy@jusdepixel.fr>
2023-03-18 20:05:00 +0100

    ci: update routes

--- routes/api.php
+++ routes/api.php
@@ -1,28 +1,24 @@
 <?php
 
 use App\Http\Controllers\Admin\DomainController;
+use App\Http\Controllers\Admin\ModuleController;
 use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Index\IndexController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', IndexController::class);
+Route::get('/domains', [DomainController::class, 'index']);
+
+Route::middleware(['auth:sanctum'])
+    ->get('/user', function (Request $request) {
+        return $request->user();
 });
 
-Route::get('/', \App\Http\Controllers\IndexController::class)->name('index');
-
-Route::prefix('/users')->group(static function() {
-    Route::get('/', [UserController::class, 'index'])->name('users@index');
-    Route::get('/{user}/edit', [UserController::class, 'edit'])->name('users@edit');
-    Route::patch('/{user}/update', [UserController::class, 'update'])->name('users@update');
-});
-
-Route::prefix('/domains')->group(static function() {
-    Route::get('/', [DomainController::class, 'index'])->name('domain@index');
-    Route::get('/{domain}', [DomainController::class, 'show'])->name('domain@show');
-    Route::get('/create', [DomainController::class, 'create'])->name('domain@create');
-    Route::get('/{slug}/edit', [DomainController::class, 'edit'])->name('domain@edit');
-    Route::post('/store', [DomainController::class, 'store'])->name('domain@store');
-    Route::post('/{domain}/destroy', [DomainController::class, 'destroy'])->name('domain@destroy');
-    Route::patch('/{domain}/update', [DomainController::class, 'update'])->name('domain@update');
+Route::prefix('/admin')
+    ->middleware(['auth'])
+    ->group(static function() {
+        Route::get('/module/{slug}', ModuleController::class);
+        Route::resource('/users', UserController::class);
+        Route::resource('/domains', DomainController::class);
 });
