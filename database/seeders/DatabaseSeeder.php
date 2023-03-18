b7e9c97783c7336fede465327eec1c7e4ff64a49
Jusdepixel <mandy@jusdepixel.fr>
2023-03-18 20:04:45 +0100

    ci: update seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,7 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
          $this->call([
-             DomainSeeder::class
+             UserSeeder::class,
+             DomainSeeder::class,
+             ModuleSeeder::class,
          ]);
     }
 }
